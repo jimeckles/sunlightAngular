36c195ac620b31a6701b2c9b4edda432bd2a637c
James Eckles <jameseckles@icloud.com>
2020-03-15 20:23:28 -0600

    completed books api integration

---
+++ src/app/services/book-service.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+export interface Book {
+  id: String;
+  title: String;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  url = 'https://8is9croova.execute-api.us-east-2.amazonaws.com/production/books';
+  //     https://8is9croova.execute-api.us-east-2.amazonaws.com/production/books
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
+
+  /*
+  body: "[{\"id\":\"60e272d88c43bbc684601096fc711dff\",\"title\":\"One Hundred Years of Solitude\"}]"
+  statusCode: 200
+*/
+
+  getBooks() {
+    return this.http.get(this.url).pipe(
+      catchError(this.handleError)
+    );
+  }
+}
